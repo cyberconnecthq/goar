d383b4b1a1fb2def52c670782b25fd3a63994f8a
zhimao <zhimao_liu@icloud.com>
2022-05-05 16:48:10 -0700

    store simple json

--- example/local_data_test.go
+++ example/local_data_test.go
@@ -1,29 +1,42 @@
 package example
 
 import (
+	"encoding/json"
+	"testing"
+
 	"github.com/everFinance/goar"
 	"github.com/everFinance/goar/types"
 	"github.com/stretchr/testify/assert"
-	"io/ioutil"
-	"testing"
 )
+
+type Box struct {
+	Width  int
+	Height int
+	Color  string
+	Open   bool
+}
 
 func Test_SendData(t *testing.T) {
 	arNode := "https://arweave.net"
-	w, err := goar.NewWalletFromPath("./wallet/account1.json", arNode) // your wallet private key
+	w, err := goar.NewWalletFromPath("../arweave-keyfile-Tmi2SGLWNkz6IEr110F1VWewauOdTjN41rZO0rQ16ig.json", arNode) // your wallet private key
 	assert.NoError(t, err)
 
-	data, err := ioutil.ReadFile("/Users/local/Downloads/abc.jpeg") // local file path
-	if err != nil {
-		panic(err)
+	box := Box{
+		Width:  10,
+		Height: 20,
+		Color:  "blue",
+		Open:   false,
 	}
+	data, _ := json.Marshal(box)
 	tags := []types.Tag{
-		{Name: "xxxx", Value: "sssss"},
-		{Name: "yyyyyy", Value: "kkkkkk"},
+		{Name: "Content-Type", Value: "application/json"},
+		{Name: "App-Name", Value: "CyberConnect"},
+		{Name: "Contributor", Value: "0x8ddD03b89116ba89E28Ef703fe037fF77451e38E"},
 	}
 	tx, err := w.SendDataSpeedUp(data, tags, 10)
 	assert.NoError(t, err)
-	t.Logf("tx hash: %s", tx.ID)
+	// https://viewblock.io/arweave/tx/RrH-QG23eKwXvuMX6vuZ94l5KTiEU6J2jgWqM3gDF3M
+	t.Errorf("tx hash: %s", tx.ID)
 }
 
 func Test_LoadData(t *testing.T) {
